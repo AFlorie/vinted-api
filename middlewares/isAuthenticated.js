585c6380536ae30c0ca73c84c169198e25428dbc
Florie Anstett <florie.anstett@gmail.com>
2020-10-23 14:33:31 +0200

    suppression du console log du token

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -2,10 +2,9 @@
 const User = require("../models/User");
 
 const isAuthenticated = async (req, res, next) => {
-  // console.log(req.headers); // authorization: 'Bearer 5f916f1726327315fcc29016',
   if (req.headers.authorization) {
     const tokenToCheck = req.headers.authorization.replace("Bearer ", "");
-    //console.log(tokenToCheck); // 5f916f1726327315fcc29016
+
     const userIdentified = await User.findOne({ token: tokenToCheck }).select(
       "account _id"
     );
